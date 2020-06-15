6d965b93e543b67e53f4260b89ddf52db4411dc2
NimVek <NimVek@users.noreply.github.com>
2020-06-15 21:51:28 +0200

    Correct travis and incremental version

---
+++ pyfugue/_version.py
@@ -0,0 +1,11 @@
+"""
+Provides pyfugue version information.
+"""
+
+# This file is auto-generated! Do not edit!
+# Use `python -m incremental.update pyfugue` to change this file.
+
+from incremental import Version
+
+__version__ = Version('pyfugue', 0, 0, 1)
+__all__ = ["__version__"]
